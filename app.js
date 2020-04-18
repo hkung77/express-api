0d287b657ab5949e3d57a74b5ef108f221fdd0c2
hkung <henrykung@live.ca>
2020-04-18 15:22:54 +0900

    add main api integration

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// CORs setup
+const allowedOrigins = [
+    'nba.hkung.me',
+    // uncomment for development purposes
+    'localhost:8080',
+    'localhost:3000',
+];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        // allow requests from mobile or curl
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) > -1) {
+            const msg = `The CORS policy for this site does not contain allowed access from the specified Origin`;
+            return callback(new Error(msg), false);
+        }
+    },
+}));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const NBA = require('nba');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-// res.render('index.html');
     res.send('hello world');
 });
+
+// NBA API Routes
+router.get('/nba/teamSearch', (req, res, next) => {
+    const term = req.query.searchTerm;
+
+    const data = NBA.teams.filter((team) => team.teamName.match(new RegExp(term)));
+    res.json({ data });
+});
+
+
+router.get('/nba/playerSearch', (req, res, next) => {
+    const term = req.query.searchTerm;
+
+    const data = NBA.players.filter(player => player.fullName.match(new RegExp(term)));
+
+    res.json({data});
+});
+
 
 module.exports = router;
