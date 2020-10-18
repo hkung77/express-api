8a49d0c0368aae7affb8718629dc1f6d15130324
Henry <henrykung@live.ca>
2020-10-18 13:16:30 +0900

    Adjust card api to return proper response

--- routes/magic-api/cards.js
+++ routes/magic-api/cards.js
@@ -1,7 +1,12 @@
-const mtgsdk = require('mtgsdk');
+const mtgsdk = require("mtgsdk");
 
 exports.getCards = (req, res) => {
-  const cards = mtgsdk.card.all({ pageSize: 1, });
-  console.log(cards);
-  res.status(200).send(cards)
-}
+  try {
+    const cards = mtgsdk.card
+      .where({ page: 1 })
+      .then((cards) => res.status(200).send(cards));
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({ error: "Unexpected Error Occured" });
+  }
+};
