31baf9fd62e4e299f7082a84df702d1fc2db635f
Henry <henrykung@live.ca>
2020-11-08 23:28:51 +0900

    Added filering support for cards

--- routes/magic-api/cards.js
+++ routes/magic-api/cards.js
@@ -3,8 +3,12 @@ const mtgsdk = require("mtgsdk");
 exports.getCards = (req, res) => {
   try {
     const page = req.query.page;
+    const colors = req.query.colors;
+    const type = req.query.type;
+    const text = req.query.text;
+
     const cards = mtgsdk.card
-      .where({ page })
+      .where({ page, colors, type, text })
       .then((cards) => res.status(200).send(cards));
   } catch (e) {
     res.status(500).send({ error: "Unexpected Error Occured" });
